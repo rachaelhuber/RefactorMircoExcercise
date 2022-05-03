cb8f2e90d5ebf4e7b09787800269699662aa9c4b
rachaelhuber <rachael.huber@lykke.com>
2022-05-03 15:44:03 +0200

    Add first test, fails randomly

---
+++ Tests/TirePressureMonitoringSystem.Tests/AlarmTests.cs
@@ -0,0 +1,19 @@
+﻿namespace TDDMicroExercises.TirePressureMonitoringSystem
+{
+    using Shouldly;
+    using Xunit;
+
+    public class AlarmTests
+    {
+        // TODO: Test runs but passes only intermitantly as we cannot control the pressure
+        [Fact]
+        public void alarm_on()
+        {
+            var alarm = new Alarm();
+
+            alarm.Check();
+
+            alarm.AlarmOn.ShouldBeTrue();
+        }
+    }
+}
