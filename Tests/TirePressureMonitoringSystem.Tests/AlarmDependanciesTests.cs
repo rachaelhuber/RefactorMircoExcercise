fca6ebf70af7be528be3fcea132dd12c08aaa259
rachaelhuber <rachael.huber@lykke.com>
2022-05-03 16:18:42 +0200

    Interesting tests to highlight class behaviour

---
+++ Tests/TirePressureMonitoringSystem.Tests/AlarmDependanciesTests.cs
@@ -0,0 +1,39 @@
+﻿namespace TDDMicroExercises.TirePressureMonitoringSystem.SomeDependencies
+{
+    using Shouldly;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Xunit;
+
+    public class AlarmDependanciesTests
+    {
+        [Fact]
+        public async Task can_instantiate_all_dependancies()
+        {
+            var types = new List<Type>
+            {
+                typeof(AnAlarmClient1),
+                typeof(AnAlarmClient2),
+                typeof(AnAlarmClient3),
+                typeof(ASensorClient),
+            };
+
+            var assertions = types.Select(x => (Action)(() =>
+            {
+                try
+                {
+                    Activator.CreateInstance(x);
+                }
+                catch (Exception ex)
+                {
+                    throw new InvalidOperationException(
+                        $"Failed to instantiate class {x.Name} {ex.Message}");
+                }
+            }));
+
+            types.ShouldSatisfyAllConditions(assertions.ToArray());
+        }    
+    }
+}
--- Tests/TirePressureMonitoringSystem.Tests/AlarmTests.cs
+++ Tests/TirePressureMonitoringSystem.Tests/AlarmTests.cs
@@ -1,6 +1,7 @@
 ﻿namespace TDDMicroExercises.TirePressureMonitoringSystem
 {
     using Shouldly;
+    using System;
     using System.Threading.Tasks;
     using Xunit;
 
@@ -17,19 +18,54 @@
         {
             var testSensor = new TestSensor();
             var alarm = new Alarm(testSensor);
-            
+
             testSensor.SetSensorValue(sensorValue);
             alarm.Check();
-            
+
             alarm.AlarmOn.ShouldBe(expected);
         }
 
+        // TODO (Rach): Desired?
+        [Fact]
+        public async Task should_stay_on_once_on()
+        {
+            var testSensor = new TestSensor();
+            var alarm = new Alarm(testSensor);
+
+            testSensor.SetSensorValue(21.000001);
+            alarm.Check();
+
+            alarm.AlarmOn.ShouldBeTrue();
+
+            testSensor.SetSensorValue(17);
+            alarm.Check();
+
+            alarm.AlarmOn.ShouldBeTrue();
+        }
+
+        // TODO (Rach): Desired?
+        [Fact]
+        public async Task should_throw_if_sensor_broken()
+        {
+            var testSensor = new TestSensor();
+            var alarm = new Alarm(testSensor);
+
+            testSensor.SetSensorValue(0);
+            Should.Throw<Exception>(() => alarm.Check());
+        }
+
+        // Note (Rach): Mock or stub could be used instead if more controlled functionality required
         private class TestSensor : ISensor
         {
             private double sensorValue;
 
             public double PopNextPressurePsiValue()
             {
+                if (sensorValue == 0)
+                {
+                    throw new Exception("Sensor is broken.");
+                }
+
                 return this.sensorValue;
             }
 
